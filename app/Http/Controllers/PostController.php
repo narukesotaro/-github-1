e6ccccbb57a695010306d3cefe93a4f68c574687
narukesotaro <narukesotaro11218080@icloud.com>
2022-12-06 17:25:52 +0900

    8-6記事の削除

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,23 +17,28 @@ class PostController extends Controller
    }
     public function create()
    {
-    return view('posts/create');
+        return view('posts/create');
    }
     public function store(Post $post, PostRequest $request)
    {
-    $input = $request['post'];
-    $post->fill($input)->save();
-    return redirect('/posts/' . $post->id);
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
    }  
-    public function edit(Post $post)
+        public function edit(Post $post)
    {
-    return view('posts/edit')->with(['post' => $post]);
+        return view('posts/edit')->with(['post' => $post]);
    }
-    public function update(PostRequest $request, Post $post)
+        public function update(PostRequest $request, Post $post)
    {
-    $input_post = $request['post'];
-    $post->fill($input_post)->save();
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
 
-    return redirect('/posts/' . $post->id);
+        return redirect('/posts/' . $post->id);
+   }
+   public function delete(Post $post)
+   {
+        $post->delete();
+        return redirect('/');
    }
 }
